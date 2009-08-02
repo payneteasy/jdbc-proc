36e47b51f27a017244a410b0d206b8c341b54fd4
esinev <esinev@fc418eb4-724a-11de-b0c7-2b5f3fb92cbb>
2009-08-02 22:38:09 +0000

    Added DECIMAL to double converter, removed UpdateEquals1 block
    git-svn-id: https://jdbc-proc.googlecode.com/svn/trunk@98 fc418eb4-724a-11de-b0c7-2b5f3fb92cbb

--- jdbc-proc/jdbc-proc-daofactory/src/main/java/com/googlecode/jdbcproc/daofactory/impl/block/factory/CallableStatementExecutorBlockFactory.java
+++ jdbc-proc/jdbc-proc-daofactory/src/main/java/com/googlecode/jdbcproc/daofactory/impl/block/factory/CallableStatementExecutorBlockFactory.java
@@ -1,7 +1,6 @@
 package com.googlecode.jdbcproc.daofactory.impl.block.factory;
 
 import com.googlecode.jdbcproc.daofactory.impl.block.ICallableStatementExecutorBlock;
-import com.googlecode.jdbcproc.daofactory.impl.block.impl.CallableStatementExecutorBlockUpdateEquals1;
 import com.googlecode.jdbcproc.daofactory.impl.block.impl.CallableStatementExecutorBlockExecute;
 import com.googlecode.jdbcproc.daofactory.impl.procedureinfo.StoredProcedureInfo;
 
@@ -13,27 +12,6 @@ import java.lang.reflect.Method;
 public class CallableStatementExecutorBlockFactory {
 
     public ICallableStatementExecutorBlock create(Method aDaoMethod, StoredProcedureInfo aProcedureInfo) {
-        // entity
-        if(BlockFactoryUtils.isGetAllMethod(aDaoMethod, aProcedureInfo)) {
-            return new CallableStatementExecutorBlockExecute();
-
-        // first id and second is entity
-        } else if(aProcedureInfo.getArgumentsCounts() > aDaoMethod.getParameterTypes().length) {
-            return new CallableStatementExecutorBlockUpdateEquals1();
-
-        // return from result set
-        } else if( ! aDaoMethod.getReturnType().equals(void.class) ) {
-            return new CallableStatementExecutorBlockExecute();
-
-        // no arguments and no returns
-        } else if ( aDaoMethod.getReturnType().equals(void.class) && aProcedureInfo.getArgumentsCounts()==0) {
-            return new CallableStatementExecutorBlockExecute();
-
-        // remove by id
-        } else if ( aDaoMethod.getReturnType().equals(void.class) && aProcedureInfo.getArgumentsCounts()==1) {
-            return new CallableStatementExecutorBlockExecute();
-        } else {
-            throw new IllegalStateException("Unsupported: "+aDaoMethod);
-        }
+        return new CallableStatementExecutorBlockExecute();
     }
 }
