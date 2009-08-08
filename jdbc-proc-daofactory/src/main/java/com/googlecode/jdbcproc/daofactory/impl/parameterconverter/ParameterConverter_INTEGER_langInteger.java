70c1c19562fe8384639c89730170184507a79418
esinev <esinev@fc418eb4-724a-11de-b0c7-2b5f3fb92cbb>
2009-08-08 20:27:49 +0000

    JP-2: added more logging, added INTEGER to langInteger and langLong converters
    git-svn-id: https://jdbc-proc.googlecode.com/svn/trunk/jdbc-proc@107 fc418eb4-724a-11de-b0c7-2b5f3fb92cbb

---
+++ jdbc-proc-daofactory/src/main/java/com/googlecode/jdbcproc/daofactory/impl/parameterconverter/ParameterConverter_INTEGER_langInteger.java
@@ -0,0 +1,39 @@
+package com.googlecode.jdbcproc.daofactory.impl.parameterconverter;
+
+import java.sql.*;
+
+/**
+ *  INTEGER - int
+ */
+public class ParameterConverter_INTEGER_langInteger implements IParameterConverter<Integer> {
+
+    public void setValue(Integer aValue, PreparedStatement aStmt, int aIndex) throws SQLException {
+        if(aValue!=null) {
+            aStmt.setInt(aIndex, aValue);
+        } else {
+            aStmt.setNull(aIndex, Types.INTEGER);
+        }
+    }
+
+    public void setValue(Integer aValue, CallableStatement aStmt, String aParameterName) throws SQLException {
+        if(aValue!=null) {
+            aStmt.setInt(aParameterName, aValue);
+        } else {
+            aStmt.setNull(aParameterName, Types.INTEGER);
+        }
+    }
+
+    public Integer getOutputParameter(CallableStatement aStmt, String aParameterName) throws SQLException {
+        Integer value = aStmt.getInt(aParameterName);
+        return aStmt.wasNull() ? null : value;
+    }
+
+    public Integer getFromResultSet(ResultSet aResultSet, String aParameterName) throws SQLException {
+        Integer value = aResultSet.getInt(aParameterName);
+        return aResultSet.wasNull() ? null : value;
+    }
+
+    public ParameterSetterKey getKey() {
+        return new ParameterSetterKey(Types.INTEGER, Integer.class);
+    }
+}
